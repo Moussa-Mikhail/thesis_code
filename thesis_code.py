56ab1dfdb9ae46f41ec19113614d0f988c205295
Moussa Mikhail <moussamikhail56@gmail.com>
2022-05-27 15:23:49 +0200

    move main definition to the top

--- thesis_code.py
+++ thesis_code.py
@@ -54,6 +54,86 @@ def time_func(func):
     return wrapper
 
 
+@time_func
+def main(
+    num_years=100.0,
+    num_steps=10**6,
+    perturbation_size=0.0,
+    perturbation_angle=None,
+    speed=1.0,
+    vel_angle=None,
+    star_mass=sun_mass,
+    planet_mass=earth_mass,
+    planet_distance=1.0,
+    lagrange_point="L4",
+    plot_conserved=False,
+):
+    """main simulates a satellite's orbit corresponding to the following parameters.
+    It then plots the orbit in inertial and corotating frames.
+
+    All parameters have default values.
+
+    It takes the following parameters:
+
+    #### Simulation parameters
+    num_years: Number of years to simulate. The default is 100.0.
+    num_steps: Number of steps to simulate. Must be an integer. The default is 10**6.
+
+    It is recommended that the ratio of num_steps to num_years
+    remains close to the ratio of default values.
+
+    #### Satellite parameters
+    perturbation_size: Size of perturbation in AU. The default is 0.0.
+    perturbation_angle: Angle of perturbation relative to positive x axis in degrees.
+    The default is None.
+    If None, then perturbation_size has the effect of
+    moving the satellite away or towards the origin.
+
+    speed: Initial speed of satellite as a factor of the planet's speed.
+    i.e. speed = 1.0 -> satellite has the same speed as the planet.
+    the default is 1.0.
+
+    vel_angle: Angle of satellite's initial velocity relative to positive x axis in degrees.
+    The default is None.
+    If None, then vel_angle is perpendicular to the satellite's default position.
+
+    lagrange_point: Non-perturbed position of satellite. String.
+    The default is 'L4' but 'L1', 'L2', 'L3', 'L5' can also be used.
+
+    #### System parameters
+    star_mass: Mass of the star in kilograms. The default is the mass of the Sun.
+    planet_mass: Mass of the planet in kilograms. The default is the mass of the Earth.
+    The constants sun_mass and earth_mass may be imported from the file constants.py.
+    planet_distance: Distance between the planet and the star in AU. The default is 1.0.
+
+    plot_conserved: If True, plots the conserved quantities:
+    energy, angular momentum, linear momentum.
+    The default is False.
+
+    This function will take ~0.5 seconds per 10**6 steps if
+    the .pyd extensions are available.
+    1.5 seconds if not.
+    The time may vary depending on your hardware.
+    It will take longer than usual on the first call.
+    """
+
+    simulation = Simulation(
+        num_years,
+        num_steps,
+        perturbation_size,
+        perturbation_angle,
+        speed,
+        vel_angle,
+        star_mass,
+        planet_mass,
+        planet_distance,
+        lagrange_point,
+        plot_conserved,
+    )
+
+    return simulation.main()
+
+
 def calc_lagrange_points(star_mass, planet_mass, planet_distance):
 
     hill_radius = planet_distance * (planet_mass / (3 * star_mass)) ** (1 / 3)
@@ -762,83 +842,3 @@ class Simulation:
         energy_plot.setLabel("left", "Normalized Energy")
 
         energy_plot.plot(times_in_years, total_energy[::arr_step] / total_energy[0] - 1)
-
-
-@time_func
-def main(
-    num_years=100.0,
-    num_steps=10**6,
-    perturbation_size=0.0,
-    perturbation_angle=None,
-    speed=1.0,
-    vel_angle=None,
-    star_mass=sun_mass,
-    planet_mass=earth_mass,
-    planet_distance=1.0,
-    lagrange_point="L4",
-    plot_conserved=False,
-):
-    """main simulates a satellite's orbit corresponding to the following parameters.
-    It then plots the orbit in inertial and corotating frames.
-
-    All parameters have default values.
-
-    It takes the following parameters:
-
-    #### Simulation parameters
-    num_years: Number of years to simulate. The default is 100.0.
-    num_steps: Number of steps to simulate. Must be an integer. The default is 10**6.
-
-    It is recommended that the ratio of num_steps to num_years
-    remains close to the ratio of default values.
-
-    #### Satellite parameters
-    perturbation_size: Size of perturbation in AU. The default is 0.0.
-    perturbation_angle: Angle of perturbation relative to positive x axis in degrees.
-    The default is None.
-    If None, then perturbation_size has the effect of
-    moving the satellite away or towards the origin.
-
-    speed: Initial speed of satellite as a factor of the planet's speed.
-    i.e. speed = 1.0 -> satellite has the same speed as the planet.
-    the default is 1.0.
-
-    vel_angle: Angle of satellite's initial velocity relative to positive x axis in degrees.
-    The default is None.
-    If None, then vel_angle is perpendicular to the satellite's default position.
-
-    lagrange_point: Non-perturbed position of satellite. String.
-    The default is 'L4' but 'L1', 'L2', 'L3', 'L5' can also be used.
-
-    #### System parameters
-    star_mass: Mass of the star in kilograms. The default is the mass of the Sun.
-    planet_mass: Mass of the planet in kilograms. The default is the mass of the Earth.
-    The constants sun_mass and earth_mass may be imported from the file constants.py.
-    planet_distance: Distance between the planet and the star in AU. The default is 1.0.
-
-    plot_conserved: If True, plots the conserved quantities:
-    energy, angular momentum, linear momentum.
-    The default is False.
-
-    This function will take ~0.5 seconds per 10**6 steps if
-    the .pyd extensions are available.
-    1.5 seconds if not.
-    The time may vary depending on your hardware.
-    It will take longer than usual on the first call.
-    """
-
-    simulation = Simulation(
-        num_years,
-        num_steps,
-        perturbation_size,
-        perturbation_angle,
-        speed,
-        vel_angle,
-        star_mass,
-        planet_mass,
-        planet_distance,
-        lagrange_point,
-        plot_conserved,
-    )
-
-    return simulation.main()
