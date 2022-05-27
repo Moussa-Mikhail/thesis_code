4f471efcc7b82b460cb5245031e5e0765041b3fd
Moussa Mikhail <moussamikhail56@gmail.com>
2022-05-27 15:20:44 +0200

    remove unnecessary helper function

--- transform_py.py
+++ transform_py.py
@@ -4,13 +4,8 @@ import numpy as np
 from numba import njit  # type: ignore
 
 
-def transform_to_corotating(sim, pos_trans):
-
-    return _transform_to_corotating(sim.times, sim.angular_speed, pos_trans)
-
-
 @njit(parallel=True)
-def _transform_to_corotating(times, angular_speed, pos_trans):
+def transform_to_corotating(times, angular_speed, pos_trans):
     # it is necessary to transform our coordinate system to one which
     # rotates with the system
     # we can do this by linearly transforming each position vector by
