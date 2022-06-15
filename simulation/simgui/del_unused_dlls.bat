e977ef986c94ac76c6fc700c8f8edc9e4979573d
Moussa Mikhail <moussamikhail56@gmail.com>
2022-06-15 16:12:14 +0200

    initial commit

---
+++ simulation/simgui/del_unused_dlls.bat
@@ -0,0 +1,11 @@
+del "dist\sim_gui\opengl32sw.dll"
+
+del "dist\sim_gui\d3dcompiler_47.dll"
+
+del "dist\sim_gui\QT5Quick.dll"
+
+del "dist\sim_gui\QT5Qml.dll"
+
+del "dist\sim_gui\libcrypto-1_1.dll"
+
+del "dist\sim_gui\libGLESv2.dll"
