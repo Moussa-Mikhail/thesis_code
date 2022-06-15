8e012f051d1a16626e44c322290c0629b1bdfa54
Moussa Mikhail <moussamikhail56@gmail.com>
2022-06-15 16:47:28 +0200

    add pylint ignore to Qtimer import

--- simulation/simulation/simulation.py
+++ simulation/simulation/simulation.py
@@ -19,7 +19,7 @@ from numpy import pi
 # shortens function call
 from numpy.linalg import norm
 from numpy.typing import NDArray
-from PyQt6.QtCore import QTimer
+from PyQt6.QtCore import QTimer  # pylint: disable=no-name-in-module
 
 from simulation.constants import AU, G, earth_mass, sat_mass, sun_mass, years
 
